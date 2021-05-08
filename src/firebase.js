6a61d23e541c77ac9e98c6eef7115b12f5d601d0
shreyaskamath7 <shreyaskamath7781@gmail.com>
2021-05-08 11:46:30 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjdbHTeKqtVTsrFiTDgsmDI56x9isuvbw",
+    authDomain: "netflix-clone-45a97.firebaseapp.com",
+    projectId: "netflix-clone-45a97",
+    storageBucket: "netflix-clone-45a97.appspot.com",
+    messagingSenderId: "84372083928",
+    appId: "1:84372083928:web:4d81fb9ed62cd2ecbfb3ad"
+  };
